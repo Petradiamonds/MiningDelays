2c503a4b6fbfb0c16355cd79dc6c6171a0fdf633
Herman Ras <herman.ras.it@gmial.com>
2022-03-28 09:13:13 +0200

    Updated to Docker

--- html/index.php
+++ html/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Mining Delays</title>
+    <title>KOF - Mining Delays</title>
 
     <!-- Chrome/android APP settings -->
     <meta name="theme-color" content="#4287f5">
