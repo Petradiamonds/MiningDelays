d450ee24f09f15a5bc4cbc9eda43e6560b29d5f4
Herman Ras <herman.ras.it@gmail.com>
2024-09-17 09:38:57 +0200

    Updated to SQl 2019 with SSL enabled.

--- html/config/db.sample.php
+++ html/config/db.sample.php
@@ -1,19 +1,18 @@
 <?php
-    $host = 'FQDN_SERVER_NAME';
-    $db   = 'DB';
-    $user = 'USER';
-    $pass = 'PASS#';
+$host = 'FQDN_SERVER_NAME';
+$db   = 'DB';
+$user = 'USER';
+$pass = 'PASS#';
 
-    $dsn = "sqlsrv:Server=$host;Database=$db";
-    $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::SQLSRV_ATTR_ENCODING    => PDO::SQLSRV_ENCODING_UTF8,
-    ];
-    try {
-        $pdo = new PDO($dsn, $user, $pass, $options);
-    } catch (\PDOException $e) {
-        throw new \PDOException($e->getMessage(), (int)$e->getCode());
-        echo 'DB_CONNECTION ERROR !';
-    }
-?>
+$dsn = "sqlsrv:Server=$host;Database=$db;Encrypt=no;TrustServerCertificate=yes;";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::SQLSRV_ATTR_ENCODING    => PDO::SQLSRV_ENCODING_UTF8,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    echo 'DB_CONNECTION ERROR !';
+}
--- html/config/db_query.php
+++ html/config/db_query.php
@@ -1,23 +1,37 @@
-<?php 
-function sqlQuery($sql,$args){
-        require("config/db.php");
-        try {
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute($args);
-        } catch (PDOException $e) {
-            echo $e->getMessage();
-        }
-
-        $results = NULL;
-        $count = 0;
-        
-        try {
-                    $results = $stmt->fetchAll();
-                    $count = $stmt->rowCount();
-        } catch (\Throwable $th) {
-                    //echo $th;
-        }
-
-        return [$results, $count];
+<?php
+function sqlQuery($sql, $args)
+{
+    require("config/db.php");
+    try {
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($args);
+    } catch (PDOException $e) {
+        //echo $e->getMessage();
+        die('<div class="container">
+                <br /><br /><br />
+                <div class="alert alert-danger" role="alert">
+                    Job Failed to be Created!
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        Transaction Error Details
+                    </div>
+                    <div class="card-body">
+                    <b>SQL Statement: </b>' . $sql . '<br />' . $e . '</div>
+                </div>
+            </div>
+            <br />');
     }
-?>
+
+    $results = NULL;
+    $count = 0;
+
+    try {
+        $results = $stmt->fetchAll();
+        $count = $stmt->rowCount();
+    } catch (\Throwable $th) {
+        // echo $th;
+    }
+
+    return [$results, $count];
+}
